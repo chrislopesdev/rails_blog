eb942706f24a57dd2b24d1cb6b5f27d5e6beb441
Chris Lopes <chris.lopes@gmail.com>
2022-02-01 22:17:40 +0000

    9.1 Basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   # CRUD order: index, show, new, edit, create, update and destroy.
   def index
     @articles = Article.all
