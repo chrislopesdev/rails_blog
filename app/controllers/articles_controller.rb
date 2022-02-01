40fbd8a41480d38a367a02002cf2a86df8833307
Chris Lopes <chris.lopes@gmail.com>
2022-02-01 17:11:16 +0000

    5.8 Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   # CRUD order: index, show, new, edit, create, update and destroy.
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
